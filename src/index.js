22dcef982032b1bf2a1742505a106335cebe21a0
OksanaStezhko <oksana_stegko@ukr.net>
2021-03-21 14:41:02 +0200

    test 7

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-const kk;
+const kdk;
 ReactDOM.render(
   <React.StrictMode>
     <App />
