6b2862a914729dc27c052fd09969d27d6458ffd2
OksanaStezhko <oksana_stegko@ukr.net>
2021-03-21 11:05:50 +0200

    Updates

---
+++ static/js/main.bfaabe1a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-group-26"]=this["webpackJsonpreact-group-26"]||[]).push([[0],{10:function(t,e,c){"use strict";c.r(e);var n=c(1),r=c.n(n),s=c(3),o=c.n(s),a=(c(8),c(0));var i=function(){return Object(a.jsx)("div",{className:"App"})};o.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))},8:function(t,e,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.bfaabe1a.chunk.js.map
