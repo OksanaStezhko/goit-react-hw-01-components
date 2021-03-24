bc51d715b1a7de095d205b1b174f7f3554452344
oksanastezhko <oksana_stegko@ukr.net>
2021-03-24 16:06:46 +0200

    сортировка файлов

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import user from './user.json';
-import statisticalData from './statistical-data.json';
-import friends from './friends.json';
-import items from './transactions.json';
-
 import React from 'react';
 
 import Statistics from './components/Statistics/Statistics';
@@ -11,15 +6,18 @@ import Profile from './components/Profile/Profile';
 import Layout from './components/Layout/Layout';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
-function App() {
-  return (
-    <Layout>
-      <Profile user={user} />
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={items} />
-    </Layout>
-  );
-}
+import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
+import friends from './data/friends.json';
+import items from './data/transactions.json';
+
+const App = () => (
+  <Layout>
+    <Profile user={user} />
+    <Statistics title="Upload stats" stats={statisticalData} />
+    <FriendList friends={friends} />
+    <TransactionHistory items={items} />
+  </Layout>
+);
 
 export default App;
